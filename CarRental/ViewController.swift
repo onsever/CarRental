11bea404bd13f9aa60e9ebbf5437c2793ac6d6cc
Onurcan Sever <onsever@icloud.com>
2021-10-22 08:13:47 -0400

    Initial Commit

---
+++ CarRental/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CarRental
+//
+//  Created by Onurcan Sever on 2021-10-22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
